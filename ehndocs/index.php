875eb29ab6c88046d2e47d06090be49663747a3a
MrRoboto <meorbass@gmail.com>
2024-05-20 22:17:14 +0200

    prueba 3

--- ehndocs/index.php
+++ ehndocs/index.php
@@ -59,7 +59,7 @@ $result2 = $con -> query($consulta2);
                 }
                 ?>
             </ul>
-            <p id="seleccionFiltro"></p>
+            <p id="seleccionFiltro">fds</p>
         </aside>
 
         <div class="contenedorNoticias">
