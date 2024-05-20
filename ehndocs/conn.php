ddf0db51c0fe6237c01b5445005acab9d8966137
Perales33 <perales.antonio33@hotmail.com>
2024-05-20 22:40:14 +0200

    Eliminar archivos repetidos

--- ehndocs/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("10.5.2.8","elhamiti_news","1234","elhamiti_news","3306");
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_errno();
-};
-mysqli_select_db($con, "elhamiti_news");
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-?>
