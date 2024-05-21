dd11c3894ab348ad172dcb0e67247446ae2d3fbb
MrRoboto <meorbass@gmail.com>
2024-05-21 22:06:53 +0200

    remove class

--- index_EN.php
+++ index_EN.php
@@ -1,5 +1,5 @@
     <body id="body">
-    <header id="header"class="lenguaje <?php echo isset($_GET['lang']) ? 'hidden' : ''; ?>">
+    <header id="header"<?php echo isset($_GET['lang']) ? 'hidden' : ''; ?>">
         <a href="./index.php" class="logo"><img src="./data/img/logo.png" alt="logo de la empresa" ></a>
         <nav>
             <ul class="url">
