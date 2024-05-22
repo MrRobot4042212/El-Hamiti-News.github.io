bfd74ce792ba1a56571af48bcddd03c2e5064083
MrRoboto <meorbass@gmail.com>
2024-05-22 12:00:06 +0200

    hotfix

--- .loginFormPost.php
+++ .loginFormPost.php
@@ -7,8 +7,8 @@ require('conn.php');
 $user = $_POST['username'];
 $pass = $_POST['password'];
 
-$sql = "SELECT user, host FROM mysql.user WHERE user = '$user' AND authentication_string = PASSWORD('$pass')";
-$result = mysqli_query($con, $sql);
+$sql = "SELECT user, host FROM mysql.user WHERE user = '$user' AND authentication_string = SHA2('$pass', 256)";
+
 
 if (mysqli_num_rows($result) > 0) {
     header("Location: ./.InsertBD.php");
