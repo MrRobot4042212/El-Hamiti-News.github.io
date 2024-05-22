db8777dc2834986fbb43b2e2ce658ea9fb665de7
MrRoboto <meorbass@gmail.com>
2024-05-22 12:31:11 +0200

    hotfixes

--- .loginFormPost.php
+++ .loginFormPost.php
@@ -8,22 +8,22 @@ $user = $_POST['username'];
 $pass = $_POST['password'];
 $host = $_POST['host'];
 
-$hashed_password = hash('sha256', $pass);
-
-$sql = "SELECT user, host, authentication_string FROM mysql.user WHERE user = '$user' AND host = '$host'";
-$result = mysqli_query($con, $sql);
+// Consulta para seleccionar el usuario y el hash de la contraseña de la base de datos
+$sql = "SELECT user, host, authentication_string FROM mysql.user WHERE user = ? AND host = ?";
+$stmt = mysqli_prepare($con, $sql);
+mysqli_stmt_bind_param($stmt, "ss", $user, $host);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 
 if ($result === false) {
     echo "Error en la consulta: " . mysqli_error($con);
 } else {
-
     if (mysqli_num_rows($result) > 0) {
-
         $row = mysqli_fetch_assoc($result);
         $stored_password_hash = $row['authentication_string'];
-        
 
-        if (hash_equals($stored_password_hash, $hashed_password)) {
+        // Verificar si la contraseña proporcionada coincide con el hash almacenado
+        if (password_verify($pass, $stored_password_hash)) {
             header("Location: ./.insertDB.php");
             exit(); 
         } else {
