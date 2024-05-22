fddd1266ec752eceb9c9f8c74709ef954e84e078
MrRoboto <meorbass@gmail.com>
2024-05-22 14:37:01 +0200

    hotfix

--- .loginFormPost.php
+++ .loginFormPost.php
@@ -23,7 +23,7 @@ if ($result && mysqli_num_rows($result) > 0) {
     if ($password === $stored_password) {
 
         $_SESSION['username'] = $username;
-        header("Location: ./.insertBD.php");
+        header("Location: ./.InsertBD.php");
         exit();
     } else {
         echo "Contraseña incorrecta";
