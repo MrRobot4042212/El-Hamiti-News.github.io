3dcd979ecf3fd39c526b5219928f94d35003c0d2
MrRoboto <meorbass@gmail.com>
2024-05-22 12:01:42 +0200

    hotfixes

--- .loginFormPost.php
+++ .loginFormPost.php
@@ -4,17 +4,28 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require('conn.php');
+// Recuperar datos del formulario
 $user = $_POST['username'];
 $pass = $_POST['password'];
 
-$sql = "SELECT user, host FROM mysql.user WHERE user = '$user' AND authentication_string = SHA2('$pass', 256)";
+// Consulta para verificar las credenciales
+$sql = "SELECT user, host FROM mysql.user WHERE user = '$user' AND authentication_string = PASSWORD('$pass')";
+$result = mysqli_query($con, $sql);
 
-
-if (mysqli_num_rows($result) > 0) {
-    header("Location: ./.InsertBD.php");
-    exit(); 
+// Verificar si la consulta se ejecutó correctamente
+if ($result === false) {
+    echo "Error en la consulta: " . mysqli_error($con);
 } else {
-    echo "Nombre de usuario o contraseña incorrectos";
+    // Verificar si se encontró un usuario con las credenciales proporcionadas
+    if (mysqli_num_rows($result) > 0) {
+        // Inicio de sesión exitoso, redirigir a otra página
+        header("Location: otra_pagina.php");
+        exit(); // Asegura que el script no siga ejecutándose después de la redirección
+    } else {
+        echo "Nombre de usuario o contraseña incorrectos";
+    }
 }
+
+// Cerrar la conexión
 mysqli_close($con);
 ?>
