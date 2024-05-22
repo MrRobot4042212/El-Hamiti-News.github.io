878ab87489243ba8afcf387f970f1c6f0fdd6cd9
Perales33 <perales.antonio33@hotmail.com>
2024-05-22 20:44:47 +0200

    hotfix

--- index_ES.php
+++ index_ES.php
@@ -71,7 +71,7 @@
             <?php 
             while ($row = $result ->  fetch_assoc()) {
                 echo '<article class="articulo tipo .'.$row['ID_noticia'].'">';
-                echo '<img class="imgNoticia" src="' . $row['urlImg'] . '" alt="">';
+                echo '<a href="' . $row['urlImg'] . '"><img class="imgNoticia" src="' . $row['urlImg'] . '" alt=""></a>';
                 echo '<div>';
                 echo '<h1 class="tituloArticulo">' . $row['Titulo_noticia'] .'</h1>';
                 echo '<h5 class="subtitulo">' . $row['Seccion_noticia'] . " - " . $row['Fecha_noticia'] . '</h5>';
