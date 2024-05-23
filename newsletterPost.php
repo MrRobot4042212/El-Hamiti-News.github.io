3c3b4faeac3e5603c499f52dc0e379f05350a593
Perales33 <perales.antonio33@hotmail.com>
2024-05-23 23:21:52 +0200

    hotfix

--- newsletterPost.php
+++ newsletterPost.php
@@ -18,7 +18,7 @@ $mail->SMTPOptions = array(
 );
 
 $mail->Host = 'email.server.elhamiti.local'; // Servidor SMTP
-$mail->SMTPSecure = 'TLS'; // Protocolo SSL o TLS
+$mail->SMTPSecure = 'tls'; // Protocolo SSL o TLS
 $mail->Port = 587; // Puerto de conexión al servidor SMTP
 $mail->SMTPAuth = true; // Para habilitar o deshabilitar la autenticación
 $mail->Username = 'admin@server.elhamiti.local'; // Usuario, normalmente el correo electrónico
