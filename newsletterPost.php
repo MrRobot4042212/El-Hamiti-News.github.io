515be4c70346e5654969e5c042e25788f2ff8b52
Perales33 <perales.antonio33@hotmail.com>
2024-05-26 15:21:13 +0200

    hotfix

--- newsletterPost.php
+++ newsletterPost.php
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->Body = $msg;
 
             if ($mail->Send()) {
-                header ('Location: ./index.php');
+                header ('Location: ./aboutUS.php');
             } else {
                 throw new Exception("Error al enviar el correo: " . $mail->ErrorInfo);
             }
